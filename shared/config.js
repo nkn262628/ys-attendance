80dd0d04a36f444e059abc609dc2c51e718e74b2
NANA <nkn262628@gmail.com>
2026-07-07 10:05:37 +0800

    新增CLIENT_SECRET

--- shared/config.js
+++ shared/config.js
@@ -8,5 +8,5 @@ const CONFIG = {
   LIFF_ID: '2010617290-IaR9AsfU',
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyBxJ_r1SWR7ebn2nipcWamFUKnuKVWfcD7RX5edoO02JKDpuAu45n16LMi9vK1AiKz3A/exec',
 
-  CLIENT_SECRET: 'YOUR_RANDOM_STRING_HERE', // 要跟 GAS Script Properties 的 CLIENT_SECRET 一致
+  CLIENT_SECRET: 'YScloud-9f3a7c2e-4b8d-41f6-a02c-d7e91b5f3c88', // 要跟 GAS Script Properties 的 CLIENT_SECRET 一致
 };
