f6ecd0223193f60af7601ffd7ba4740526b9e0a1
NANA <nkn262628@gmail.com>
2026-07-08 10:19:20 +0800

    優化placeholder

--- shared/config.js
+++ shared/config.js
@@ -4,9 +4,8 @@
    ============================================ */
 
 const CONFIG = {
-  // 從 LINE Developers「Add a LIFF app」建立後複製貼上
   LIFF_ID: '2010617290-IaR9AsfU',
   GAS_URL: 'https://script.google.com/macros/s/AKfycbyBxJ_r1SWR7ebn2nipcWamFUKnuKVWfcD7RX5edoO02JKDpuAu45n16LMi9vK1AiKz3A/exec',
-
-  CLIENT_SECRET: 'YScloud-9f3a7c2e-4b8d-41f6-a02c-d7e91b5f3c88', // 要跟 GAS Script Properties 的 CLIENT_SECRET 一致
+  CLIENT_SECRET: 'YScloud-9f3a7c2e-4b8d-41f6-a02c-d7e91b5f3c88',
+  EMP_ID_EXAMPLE: 'EMP-001', // 換客戶時，員工編號範例格式改這裡
 };
